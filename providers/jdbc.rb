67acda373d0068936c8bf16e15074c383793b15d
Peter Halliday <pghalliday@gmail.com>
2014-08-29 10:27:53 +0200

    fixed jdbc nil checks

--- providers/jdbc.rb
+++ providers/jdbc.rb
@@ -5,16 +5,16 @@ end
 action :update do
   original_file = ::File.read(new_resource.file)
   file = Chef::Util::FileEdit.new(new_resource.file)
-  if nil?(original_file.match(/^sonar\.jdbc\.username=#{Regexp.escape(new_resource.username)}$/))
+  if original_file.match(/^sonar\.jdbc\.username=#{Regexp.escape(new_resource.username)}$/).nil?
     file.search_file_replace_line(/^sonar\.jdbc\.username=/, "sonar.jdbc.username=#{new_resource.username}")
   end
-  if nil?(original_file.match(/^sonar\.jdbc\.password=#{Regexp.escape(new_resource.password)}$/))
+  if original_file.match(/^sonar\.jdbc\.password=#{Regexp.escape(new_resource.password)}$/).nil?
     file.search_file_replace_line(/^sonar\.jdbc\.password=/, "sonar.jdbc.pasword=#{new_resource.password}")
   end
-  if nil?(original_file.match(/^# Database URL$/))
+  if original_file.match(/^# Database URL$/).nil?
     file.search_file_replace_line(/^# Comment the following line to deactivate the default embedded database\./, '# Database URL')
   end
-  if nil?(original_file.match(/^sonar\.jdbc\.url=#{Regexp.escape(new_resource.url)}$/))
+  if original_file.match(/^sonar\.jdbc\.url=#{Regexp.escape(new_resource.url)}$/).nil?
     file.search_file_replace_line(/^sonar\.jdbc\.url=/, "sonar.jdbc.url=#{new_resource.url}")
   end
   file.write_file
